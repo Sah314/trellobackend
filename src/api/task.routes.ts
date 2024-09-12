a74b478bdf062cea51c6cf95c12a822e003b80c3
Sahil Khadayate <khadayatesahil@gmail.com>
2024-09-12 13:16:17 +0530

    feat: Add new routes for task and user routes

---
+++ src/api/task.routes.ts
@@ -0,0 +1,16 @@
+import express ,{Request,Response,NextFunction} from 'express';
+
+const router = express.Router();
+
+// endpoints
+
+router.post("/products", async (req: Request, res: Response, next: NextFunction) => {
+try {
+    
+} catch (error) {
+    
+}
+}
+);
+
+export default router;
---
+++ src/api/user.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+// endpoints
+
+
+
+export default router;
